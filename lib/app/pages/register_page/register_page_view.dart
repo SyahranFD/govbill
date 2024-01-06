0a21e58526b9c39bce24484a0955c4460b7b76ea
Adipp <emangadayanglain@gmail.com>
2024-01-06 16:48:37 +0700

    [A] Add style register screen

--- lib/app/pages/register_page/register_page_view.dart
+++ lib/app/pages/register_page/register_page_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:govbill/app/pages/register_page/widget/register_form_widget.dart';
 import 'package:govbill/common/helper/themes.dart';
 
 class RegisterPageView extends StatelessWidget {
@@ -10,10 +12,63 @@ class RegisterPageView extends StatelessWidget {
       backgroundColor: backgroundPageColor,
       body: Container(
         width: double.infinity,
+        margin: EdgeInsets.symmetric(horizontal: 15),
         child: SingleChildScrollView(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                alignment: Alignment.topRight,
+                margin: EdgeInsets.only(right: 20),
+                child: Image.asset(
+                  "assets/images/logo.png",
+                ),
+              ),
+              SizedBox(
+                height: 22,
+              ),
+              Text(
+                "Daftar Akun",
+                style: tsHeadlineSmallBoldDarkBlue,
+              ),
+              Text(
+                "Daftarkan akun anda dengan mengisi kolom dibawah ini",
+                style: tsBodyMediumRegularDarkGrey,
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              RegisterFormWidget(
+                hintText: "Email",
+                iconPrefix: SvgPicture.asset("assets/icons/icEmail.svg"),
+                isObsecure: false,
+              ),
+              RegisterFormWidget(
+                hintText: "No. Telepon",
+                iconPrefix: SvgPicture.asset("assets/icons/icPhone.svg"),
+                isObsecure: false,
+              ),
+              RegisterFormWidget(
+                hintText: "Password",
+                iconPrefix: SvgPicture.asset("assets/icons/icLock.svg"),
+                isObsecure: false,
+              ),
+              RegisterFormWidget(
+                hintText: "Password",
+                iconPrefix: SvgPicture.asset("assets/icons/icLock.svg"),
+                isObsecure: false,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              GestureDetector(
+                child: Container(
+                  child: Text("Daftar Sekarang "),
+                ),
+              )
             ],
           ),
         ),
---
+++ lib/app/pages/register_page/widget/register_form_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:govbill/common/helper/themes.dart';
+
+class RegisterFormWidget extends StatelessWidget {
+  Widget? iconPrefix;
+  String? hintText;
+  bool? isObsecure;
+  Widget? iconSuffix;
+
+  RegisterFormWidget({
+    Key? key,
+    this.iconPrefix,
+    this.hintText,
+    this.isObsecure,
+    this.iconSuffix,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      margin: EdgeInsets.only(bottom: 12),
+      child: TextFormField(
+        decoration: InputDecoration(
+            prefixIconConstraints: BoxConstraints(
+              minWidth: 60,
+              minHeight: 30,
+            ),
+            contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            prefixIcon: Container(
+                height: 20,
+                width: 20,
+                margin: EdgeInsets.only(left: 15, right: 15),
+                child: iconPrefix),
+            fillColor: primaryColor,
+            filled: true,
+            hintText: hintText,
+            border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(10)),
+            hintStyle: tsBodyMediumRegularDarkGrey),
+      ),
+    );
+  }
+}
