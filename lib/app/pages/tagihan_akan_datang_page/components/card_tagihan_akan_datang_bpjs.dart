41e3455628de2a5a40397585ee619a8d8aea7767
SyahranFD <fadhilrafa1@gmail.com>
2024-01-08 18:19:16 +0700

    feat: [SF] add card tagihan akan datang for each category

---
+++ lib/app/pages/tagihan_akan_datang_page/components/card_tagihan_akan_datang_bpjs.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:govbill/common/helper/themes.dart';
+
+class CardTagihanAkanDatangBPJS extends StatelessWidget {
+
+  CardTagihanAkanDatangBPJS(
+      {Key? key,
+        required this.noVA,
+        required this.jenisTagihan,
+        required this.namaPelanggan,
+        required this.waktuBayar,
+        required this.hargaRupiah,
+      });
+
+  final String noVA;
+  final String jenisTagihan;
+  final String namaPelanggan;
+  final String waktuBayar;
+  final String hargaRupiah;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size mediaQuery = MediaQuery.of(context).size;
+    final double width = mediaQuery.width;
+
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+          color: primaryColor,
+          borderRadius: BorderRadius.circular(10)
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            right: 0,
+            bottom: 0,
+            child: SvgPicture.asset(
+              "assets/icons/icShadow$jenisTagihan.svg",
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 15, bottom: 20, left: 15, right: 15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("No. VA : $noVA", style: tsLabelRegularDarkBlue),
+
+                SizedBox(height: 10),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: categoryBPJS,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0x19000000),
+                            blurRadius: 10,
+                            offset: Offset(0, 0),
+                            spreadRadius: 0,
+                          )
+                        ],
+                      ),
+                      width: width * 0.1,
+                      height: width * 0.1,
+                      alignment: Alignment.center,
+                      child: SvgPicture.asset(
+                        "assets/icons/icKategoriBPJS.svg",
+                        width: 24,
+                      ),
+                    ),
+                    SizedBox(width: width * 0.04),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                            width: width * 0.685,
+                            child: Text("Tagihan BPJS $namaPelanggan", style: tsBodySmallSemiboldBlack)
+                        ),
+                        SizedBox(height: 4),
+                        Text("Akan dibayar tanggal $waktuBayar", style: tsLabelRegularBlack),
+                        SizedBox(height: 4),
+                        Text("$hargaRupiah", style: tsLabelRegularRed),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/app/pages/tagihan_akan_datang_page/components/card_tagihan_akan_datang_pln.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:govbill/common/helper/themes.dart';
+
+class CardTagihanAkanDatangPLN extends StatelessWidget {
+
+  CardTagihanAkanDatangPLN(
+      {Key? key,
+        required this.idPelanggan,
+        required this.jenisTagihan,
+        required this.namaPelanggan,
+        required this.waktuBayar,
+        required this.hargaRupiah,
+      });
+
+  final String idPelanggan;
+  final String jenisTagihan;
+  final String namaPelanggan;
+  final String waktuBayar;
+  final String hargaRupiah;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size mediaQuery = MediaQuery.of(context).size;
+    final double width = mediaQuery.width;
+
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+          color: primaryColor,
+          borderRadius: BorderRadius.circular(10)
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            right: 0,
+            bottom: 0,
+            child: SvgPicture.asset(
+              "assets/icons/icShadow$jenisTagihan.svg",
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 15, bottom: 20, left: 15, right: 15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("ID Pelanggan : $idPelanggan", style: tsLabelRegularDarkBlue),
+
+                SizedBox(height: 10),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: categoryPLN,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0x19000000),
+                            blurRadius: 10,
+                            offset: Offset(0, 0),
+                            spreadRadius: 0,
+                          )
+                        ],
+                      ),
+                      width: width * 0.1,
+                      height: width * 0.1,
+                      alignment: Alignment.center,
+                      child: SvgPicture.asset(
+                        "assets/icons/icKategori$jenisTagihan.svg",
+                        width: 24,
+                      ),
+                    ),
+                    SizedBox(width: width * 0.04),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                            width: width * 0.685,
+                            child: Text("Tagihan $jenisTagihan $namaPelanggan", style: tsBodySmallSemiboldBlack)
+                        ),
+                        SizedBox(height: 4),
+                        Text("Akan dibayar tanggal $waktuBayar", style: tsLabelRegularBlack),
+                        SizedBox(height: 4),
+                        Text("$hargaRupiah", style: tsLabelRegularRed),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
